a03ea598237b9a6ac2743456a4886a1623492e32
Julia Simakina <simakina.js@yandex.ru>
2024-06-10 15:09:05 +0300

    pagination with pages

--- src/controllers/book/getAllBooks.ts
+++ src/controllers/book/getAllBooks.ts
@@ -2,20 +2,34 @@ import { Request, Response, NextFunction } from "express";
 import bookRepository from "../../db/bookRepository";
 import Book from "../../db/entity/Book";
 
+type GetBooksResponseType = {
+  slicedCards: Book[];
+  numbers: number[];
+};
+
 export default async function getAllBooks(
   req: Request,
-  res: Response<Book[]>,
+  res: Response<GetBooksResponseType>,
   next: NextFunction
 ) {
   try {
-    const startIndex = Number(req.query.startIndex);
-    const endIndex = Number(req.query.endIndex);
+    const itemsPerPage = Number(req.query.itemsPerPage);
+    const currentPage = Number(req.query.currentPage);
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
 
     const [books, booksCount] = await bookRepository.findAndCount();
 
+    let numbers: number[] = [];
+
+    for (let i = 1; i <= Math.ceil(booksCount / itemsPerPage); i++) {
+      numbers.push(i);
+    }
+
     const slicedCards = books.slice(startIndex, endIndex);
 
-    return res.json(slicedCards);
+    return res.json({ slicedCards, numbers });
   } catch (error) {
     console.error(error);
     throw error;
